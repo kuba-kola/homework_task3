9522357a0e9793f4af5c62b5cdcb048cc15df254
Yakau Trafimovich <5243212@gmail.com>
2022-03-13 19:00:03 +0100

    13.03

--- task3.js
+++ task3.js
@@ -1,1 +1,23 @@
-const obj = {};
+function makeObjectDeepCopy (obj) {
+    const deepClone = {};
+
+    if (typeof obj !== 'object' || obj === null) {
+        return obj;
+      }
+    
+    for (key in obj) {
+        deepClone[key] = makeObjectDeepCopy(obj[key]);
+    }
+
+    return deepClone;
+}
+
+function selectFromInterval(arr, arg1, arg2) {
+    const isCorrect = Array.isArray(arr) && array.every((el) => Number.isFinite(el)) && Number.isFinite(arg1) && Number.isFinite(arg2);
+    const newArr =[];
+    const res = [];
+
+    if (!isCorrect) {
+        throw new Error ('Ошибка! Не корректное значение!')
+    }
+}
