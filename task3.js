fa80461448ec4055b5cfb669e82e176140c1116a
Yakau Trafimovich <5243212@gmail.com>
2022-03-14 11:28:46 +0100

    add 3 task

--- task3.js
+++ task3.js
@@ -13,11 +13,41 @@ function makeObjectDeepCopy (obj) {
 }
 
 function selectFromInterval(arr, arg1, arg2) {
-    const isCorrect = Array.isArray(arr) && array.every((el) => Number.isFinite(el)) && Number.isFinite(arg1) && Number.isFinite(arg2);
-    const newArr =[];
-    const res = [];
+    const correctArgs = Number.isFinite(arg1) && Number.isFinite(arg2);
+    const correctArr = Array.isArray(arr) && array.every((el) => Number.isFinite(el));
+    const isCorrect = correctArgs && correctArr;
+    const res = array.filter( (el) => el <= arg1 && el >= arg2 );
+    
+    if (!isCorrect) {
+        throw new Error ('Ошибка!');
+    }
+    
+    return res;
+}
+
+const myIterable = {
+    from: 1,
+    to: 4
+};
+
+myIterable[Symbol.iterator] = function*() {
+    const correctArgs = Number.isFinite(this.from) && Number.isFinite(this.to);
+    const isCorrect = correctArgs && this.to > this.from;
 
     if (!isCorrect) {
-        throw new Error ('Ошибка! Не корректное значение!')
+        throw new Error ('Ошибка!');
+    }
+
+    return {
+      current: this.from,
+      last: this.to,
+
+      next() {
+        if (this.current <= this.last) {
+          return { value: this.current++, done: false };
+        }
+
+        return { done: true };
+      }
     }
 }
